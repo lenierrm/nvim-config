dddaaa5f3390eeb39d3dd9e1710c4c75f6916d49
lenierrm <lenier.ruiz@gmail.com>
2025-11-11 17:48:45 -0500

    feature: clipboard option added

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+vim.cmd("set clipboard=unnamedplus")
 vim.g.mapleader = " "
 
 -- Navigate vim panes better
