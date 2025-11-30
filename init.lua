fa969ad40b30d36789347a377bd3f196d4105282
lenierrm <lenier.ruiz@gmail.com>
2025-11-30 11:36:48 -0500

    feature: update screen when file is edited on disk

--- init.lua
+++ init.lua
@@ -1,21 +1,31 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")
 
-
 -- Cargar configuración LSP
 require("lsp.init")
 
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { noremap = true, silent = true })
+
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+	command = "if mode() != 'c' | checktime | endif",
+	pattern = { "*" },
+})
+
+vim.api.nvim_create_autocmd({ "FileChangedShellPost" }, {
+	command = 'echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None',
+	pattern = { "*" },
+})
