177619664e947f1f3855384f4fe222ababf0feae
lenierrm <lenier.ruiz@gmail.com>
2025-12-21 09:59:20 -0500

    feat: add cursorline setting

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,6 +5,7 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set clipboard=unnamedplus")
+vim.cmd("set cursorline")
 vim.g.mapleader = " "
 
 -- Navigate vim panes better
