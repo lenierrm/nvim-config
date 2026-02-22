3c22e9c0544a51e08c7dbfd26e9d5754fe5f1087
lenierm <lenier.ruiz@gmail.com>
2026-02-21 20:45:22 -0500

    fix: adding new modules

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,21 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_installed = {
-              "javascript", "html", "php", "css", --"json", "sql", "lua"
-            },
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
-} 
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = {
+				"javascript",
+				"php",
+				"css",
+				"markdown",
+				"markdown_inline", --"json", "sql", "lua"
+				"html",
+			},
+			highlight = {
+				enable = true,
+			},
+			indent = { enable = true },
+		})
+	end,
+}
